d9d73b1cbc88ad513437f0e1062ae22ea91c5429
Administrator <13631155285@163.com>
2026-09-08 15:55:19 +0800

    网关和授权模块创建

---
+++ GatewayModule/src/main/java/org/leon/gatewaymodule/GatewayModuleApplication.java
@@ -0,0 +1,13 @@
+package org.leon.gatewaymodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayModuleApplication.class, args);
+    }
+
+}
---
+++ GatewayModule/src/test/java/org/leon/gatewaymodule/GatewayModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.leon.gatewaymodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
